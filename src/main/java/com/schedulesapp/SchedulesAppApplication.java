6bed826c10741dec6cfd84a0a1d219a6ec88ab03
pcb2002 <machat350@gmail.com>
2026-04-09 16:01:47 +0900

    [Build]: setting

---
+++ src/main/java/com/schedulesapp/SchedulesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulesAppApplication.class, args);
+    }
+
+}
